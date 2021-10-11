3c6e8bc36ed8fe940c671f9835a96f6722f75feb
Penny Tague <pmtague9@gmail.com>
2021-10-11 10:55:45 -0700

    Updates to books model to more completely define the class and changes to routes to jsonify the data returned

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy, request
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://beobuojhegamsi:0d03035ef88099e1bd219b3772e17522354a9ac58068766ef6ac180fe38a83ec@ec2-52-3-130-181.compute-1.amazonaws.com:5432/d8gbvgrngr0fsa'
-db = SQLAlchemy(app)
 
-# Create model for database, limiting to data that will be needed
+# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 class BooksModel(db.Model):
     __tablename__ = 'books'
 
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.String())
     title = db.Column(db.String())
     price = db.Column(db.Float())
     year = db.Column(db.String())
+
+    def __init__(self, author, title, price, year):
+        self.author = author
+        self.title = title
+        self.price = price
+        self.year = year
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'title': self.title,
+            'price': self.price
+        }
 
 
 @app.route('/')
@@ -23,21 +39,22 @@ def main():
     return "Hello World!"
 
 
-# @app.route('/books/')
-# def book(id):
-#     books = BooksModel.query.filter_by(id='id')
-#     return {books}
+@app.route('/books/')
+def books():
+    try:
+        books = BooksModel.query.all()
+        return jsonify([book.serialize() for book in books])
+    except:
+        return(str(Exception))
 
 
-@app.route('/books/<int:id>')
+@app.route('/books/<id>')
 def book_info(id):
-    books = BooksModel.query.filter_by(id=id)
-    print(books)
-    return {
-        'title': BooksModel.title,
-        'price': BooksModel.price,
-        'method': request.method
-    }
+    try:
+        book = BooksModel.query.filter_by(id=id)
+        return jsonify(book.serialize())
+    except:
+        return(str(Exception))
 
 
 if __name__ == "__main__":
