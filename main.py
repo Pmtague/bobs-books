2249adc3bffdc3b614f5d995121abbbb375d5423
Penny Tague <pmtague9@gmail.com>
2021-10-11 12:11:23 -0700

    Established that postgres DB connection is working, able to query and retrieve title and price for all books

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# app.config.from_object(os.environ['APP_SETTINGS'])
+load_dotenv()
+
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
